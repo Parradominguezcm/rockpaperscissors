091cf7438e0fd6c3b8abd7788be8f71997708703
Clara <cmarinaparra@gmail.com>
2023-08-07 17:51:04 +0100

    refactoring for scalability

---
+++ src/config.js
@@ -0,0 +1,15 @@
+const config = {
+    rock: {
+        'scissors': true,
+        'paper': false
+    },
+    paper: {
+        'scissors': false,
+        'rock': true
+    },
+    scissors: {
+        'paper': true,
+        'rock': false
+    }
+}
+module.exports = config;
